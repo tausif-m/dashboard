7edae7b27e2d1bd619bbe01138045dd84e492a85
Tausif Mukhtar <email>
2022-12-09 11:12:24 +0530

    Initial Commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Button = (props) => {
+  return (
+    <button
+      type="button"
+      style={{
+        backgroundColor: props.bgColor,
+        color: props.color,
+        borderRadius: props.borderRadius,
+      }}
+      className={`text-${props.size} p-3 hover:drop=shadow-xl`}
+    >
+      {props.text}
+    </button>
+  );
+};
+
+export default Button;
